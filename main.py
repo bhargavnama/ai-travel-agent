6cf6dec0e588a9addf13bbcfa8a86800ce71cd8f
bhargavnama <bhargavnama143@gmail.com>
2026-06-24 06:28:24 +0530

    created streamlit frontend.

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-def main():
-    print("Hello from ai-trip-planner!")
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.responses import JSONResponse
+from agent.agentic_workflow import GraphBuilder
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+class QueryRequest(BaseModel):
+    query: str
+
+@app.post('/query')
+def quer_travel_agent(query: QueryRequest):
+    try:
+        graph = GraphBuilder( model_provider = 'groq' )
+        react_app = graph()
+
+        png_graph = react_app.get_graph().get_mermaid_png()
+        with open('my_graph.png', 'wb') as f:
+            f.write(png_graph)
+
+        messages = {
+            'messages': [query.question]
+        }
+        output = react_app.invoke(messages)
+
+        if isinstance( output, dict ) and 'messages' in output:
+            final_output = output['messages'][-1].content
+        else:
+            final_output = str(output)
+
+        return {
+            'answer': final_output
+        }
+    except Exception as e:
+        return JSONResponse( status_code=500, content={ 'error': str(e) } )
